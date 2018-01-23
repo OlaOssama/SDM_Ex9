e32e4e9b33d660584eb2438093881a20054f97e3
Nam Nguyen <Nam Nguyen@NamNguyen-Notebook>
2018-01-23 16:04:41 +0100

    Send implemented

--- SDM_CodeBase_Exercise_09_2017/src/de/tuda/sdm/dmdb/sql/operator/exercise/Receive.java
+++ SDM_CodeBase_Exercise_09_2017/src/de/tuda/sdm/dmdb/sql/operator/exercise/Receive.java
@@ -1,6 +1,8 @@
 package de.tuda.sdm.dmdb.sql.operator.exercise;
 
 import java.io.IOException;
+import java.util.Queue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import de.tuda.sdm.dmdb.net.TCPServer;
 import de.tuda.sdm.dmdb.sql.operator.Operator;
@@ -9,6 +11,7 @@ import de.tuda.sdm.dmdb.storage.AbstractRecord;
 
 /**
  * Implementation of receive operator
+ * 
  * @author melhindi
  *
  */
@@ -16,10 +19,16 @@ public class Receive extends ReceiveBase {
 
 	/**
 	 * Constructor of Receive
-	 * @param child - Child operator used to process next calls, usually SendOperator
-	 * @param numPeers - Number of peer nodes that have to finish processing before operator finishes
-	 * @param listenerPort - Port on which to bind receive server
-	 * @param nodeId - Own nodeId, used for debugging
+	 * 
+	 * @param child
+	 *            - Child operator used to process next calls, usually SendOperator
+	 * @param numPeers
+	 *            - Number of peer nodes that have to finish processing before
+	 *            operator finishes
+	 * @param listenerPort
+	 *            - Port on which to bind receive server
+	 * @param nodeId
+	 *            - Own nodeId, used for debugging
 	 */
 	public Receive(Operator child, int numPeers, int listenerPort, int nodeId) {
 		super(child, numPeers, listenerPort, nodeId);
@@ -29,22 +38,34 @@ public class Receive extends ReceiveBase {
 	public void open() {
 		// TODO: implement this method
 		// HINT: local cache must be passed to TCPServer
-		//       and will be accessed by multiple Handler-Threads - take multi-threading into account where applicable!
-				
+		// and will be accessed by multiple Handler-Threads - take multi-threading into
+		// account where applicable!
+
 		// init local cache
 
-		// Attention: call open on child after starting receive server, so that sendOperator can connect
+		// Attention: call open on child after starting receive server, so that
+		// sendOperator can connect
 
+		Queue<AbstractRecord> localCache = null;
+		AtomicInteger finishedPeers = new AtomicInteger(numPeers-1);
+		try {
+			TCPServer self = new TCPServer(listenerPort,localCache,finishedPeers);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public AbstractRecord next() {
 		// TODO: implement this method
 		// HINT: local cache must be passed to TCPServer
-		//       and will be accessed by multiple Handler-Threads - take multi-threading into account where applicable!
+		// and will be accessed by multiple Handler-Threads - take multi-threading into
+		// account where applicable!
 		// process local and received records...
-		
-			// check if we finished processing of all records - hint: you can use this.finishedPeers
+
+		// check if we finished processing of all records - hint: you can use
+		// this.finishedPeers
 
 		return null;
 	}
--- SDM_CodeBase_Exercise_09_2017/src/de/tuda/sdm/dmdb/sql/operator/exercise/Send.java
+++ SDM_CodeBase_Exercise_09_2017/src/de/tuda/sdm/dmdb/sql/operator/exercise/Send.java
@@ -2,15 +2,24 @@ package de.tuda.sdm.dmdb.sql.operator.exercise;
 
 import java.io.IOException;
 import java.net.ConnectException;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import de.tuda.sdm.dmdb.net.TCPClient;
+import de.tuda.sdm.dmdb.net.TCPServer;
 import de.tuda.sdm.dmdb.sql.operator.Operator;
 import de.tuda.sdm.dmdb.sql.operator.SendBase;
 import de.tuda.sdm.dmdb.storage.AbstractRecord;
+import de.tuda.sdm.dmdb.storage.Record;
+import de.tuda.sdm.dmdb.storage.types.exercise.SQLInteger;
 
 /**
  * Implementation of send operator
+ * 
  * @author melhindi
  *
  */
@@ -18,45 +27,89 @@ public class Send extends SendBase {
 
 	/**
 	 * Constructor of Send
-	 * @param child - Child operator used to process next calls, e.g., TableScan or Selection
-	 * @param nodeId - Own nodeId to identify which records to keep locally
-	 * @param nodeMap - Map containing connection information (as "IP:port" or "domain-name:port") to establish connection to other peers
-	 * @param partitionColumn - Number of column that should be used to repartition the data
+	 * 
+	 * @param child
+	 *            - Child operator used to process next calls, e.g., TableScan or
+	 *            Selection
+	 * @param nodeId
+	 *            - Own nodeId to identify which records to keep locally
+	 * @param nodeMap
+	 *            - Map containing connection information (as "IP:port" or
+	 *            "domain-name:port") to establish connection to other peers
+	 * @param partitionColumn
+	 *            - Number of column that should be used to repartition the data
 	 */
-	public Send(Operator child, int nodeId, Map<Integer,String> nodeMap, int partitionColumn) {
+	public Send(Operator child, int nodeId, Map<Integer, String> nodeMap, int partitionColumn) {
 		super(child, nodeId, nodeMap, partitionColumn);
 	}
 
+	Map<Integer, TCPClient> socketMap;
+	
 	@Override
 	public void open() {
 		// TODO: implement this method
 		// init child
-
+		child.open();
 		// create a client socket for all peer nodes using information in nodeMap
 		// store client socket in map for later use
+
+		socketMap = new HashMap<Integer, TCPClient>();
+
+		for (Map.Entry<Integer, String> entry : nodeMap.entrySet()) {
+			if (entry.getKey() != nodeId) {
+				String address = nodeMap.get(nodeId);
+				String hostname = address.substring(0, address.indexOf(":"));
+				int port = Integer.valueOf(address.substring(address.indexOf(":")));
+
+				try {
+					socketMap.put(entry.getKey(), new TCPClient(hostname, port));
+
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 
 	}
 
 	@Override
 	public AbstractRecord next() {
 		// TODO: implement this method
-		// retrieve next from child and determine whether to keep record local or send to peer
-
-				// store locally
-
-				// send to a peer
-
+		// retrieve next from child and determine whether to keep record local or send
+		// to peer
 
 		// reached end, close connections to peers
-
-
-		return null;
+		int n = nodeMap.size();
+		
+		AbstractRecord rec;
+		Queue<AbstractRecord> resultList =  new LinkedList<AbstractRecord>();
+		
+		do {
+			rec = child.next();
+			if (rec != null) {
+				int id = ((SQLInteger) rec.getValue(partitionColumn)).getValue() % n; // nodeId of peer to send
+				if (id == (nodeId % n)) { // store locally
+					resultList.add(rec);
+				} else { // send to a peer
+					TCPClient peer = socketMap.get(id);
+					peer.sendRecord(rec);
+				}
+			}
+		} while (rec != null);
+		return resultList.remove();
 	}
 
 	@Override
 	public void close() {
 		// TODO: implement this method
-		// reverse what was done in open() - hint there is a helper method that you can use
+		// reverse what was done in open() - hint there is a helper method that you can
+		// use
+
+		child.close();
 	}
 
 }
